4f71ef5683cb8f3ef3ff6a28b4302e019c29a7f1
Igor Siergiej <igorsiergiej@gmail.com>
2024-06-12 19:49:54 +0100

    1: added initial react app with authorisation

---
+++ web/src/components/Auth/index.tsx
@@ -0,0 +1,30 @@
+import { CircularProgress } from '@mui/material';
+import { toRelativeUrl } from '@okta/okta-auth-js';
+import { useOktaAuth } from '@okta/okta-react';
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+
+export const RequiredAuth: React.FC = () => {
+    const { oktaAuth, authState } = useOktaAuth();
+
+    useEffect(() => {
+        if (!authState) {
+            return;
+        }
+
+        if (!authState?.isAuthenticated) {
+            const originalUri = toRelativeUrl(
+                window.location.href,
+                window.location.origin
+            );
+            oktaAuth.setOriginalUri(originalUri);
+            oktaAuth.signInWithRedirect();
+        }
+    }, [oktaAuth, !!authState, authState?.isAuthenticated]);
+
+    if (!authState || !authState?.isAuthenticated) {
+        return <CircularProgress />;
+    }
+
+    return <Outlet />;
+};
