749255cae7546ee166961cfa957ac6e4917af8d2
Igor Siergiej <igorsiergiej@gmail.com>
2024-12-06 20:29:20 +0000

    Fixed incorrect npm registry and added bucket secrets

--- web/src/api/getApiUrl.ts
+++ web/src/api/getApiUrl.ts
@@ -1,8 +1,6 @@
 export const getApiUrl = () => {
     const currentUrl = window.origin;
 
-    return 'http://localhost:3001';
-
     if (currentUrl === 'http://localhost:3000') {
         return 'https://jewellerycatalogue.onthewifi.com';
     } else {
---
+++ web/src/components/Alert/index.tsx
@@ -0,0 +1,7 @@
+import { AlertTitle, Alert } from '@mui/material';
+
+export interface IAlertProps {}
+
+const CustomAlert: React.FC<IAlertProps> = () => {
+    return <div>hi</div>;
+};
