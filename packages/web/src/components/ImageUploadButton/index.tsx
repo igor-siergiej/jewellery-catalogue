6be0b5903bd873a1e02152ced7a48587f03a7d3e
Igor Siergiej <igorsiergiej@gmail.com>
2025-04-07 08:57:26 +0100

    added image upload preview

--- packages/web/src/components/ImageUploadButton/index.tsx
+++ packages/web/src/components/ImageUploadButton/index.tsx
@@ -3,29 +3,40 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import useStyles from './index.styles';
 import { UseFormRegister } from 'react-hook-form';
 import { FormDesign } from '@jewellery-catalogue/types';
+import { useState } from 'react';
 
 export interface ImageUploadButtonProps {
-    register: UseFormRegister<FormDesign>
+    register: UseFormRegister<FormDesign>;
 }
 
 const ImageUploadButton: React.FC<ImageUploadButtonProps> = ({ register }) => {
     const { classes } = useStyles();
+    const [selectedImage, setSelectedImage] = useState<string>();
 
     return (
-        <Button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            startIcon={<CloudUploadIcon />}
-        >
-            Upload files
-            <input
-                {...register('image')}
-                className={classes.upload}
-                type="file"
-            />
-        </Button>
+        <div>
+            <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+            >
+                Upload files
+                <input
+                    {...register('image')}
+                    onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        setSelectedImage(file ? URL.createObjectURL(file) : undefined);
+                    }}
+                    className={classes.upload}
+                    type="file"
+                />
+            </Button>
+
+            {selectedImage && (<img src={selectedImage} />)}
+
+        </div>
     );
 };
 
