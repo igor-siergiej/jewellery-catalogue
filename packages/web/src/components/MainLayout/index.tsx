5c4d0af9568fd69c6cd0bc41c87c99c116710c70
Igor Siergiej <igorsiergiej@gmail.com>
2025-07-11 20:15:01 +0100

    removed box/box

--- packages/web/src/components/MainLayout/index.tsx
+++ packages/web/src/components/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box/Box';
+import Box from '@mui/material/Box';
 import { ReactNode } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../NavBar';
