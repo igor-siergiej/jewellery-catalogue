143462eb7e57b0bd9c48eea1d468c85c54981bac
Igor Siergiej <igorsiergiej@gmail.com>
2025-07-13 18:28:02 +0100

    trigger new pipeline

--- packages/web/src/context/UserContext.tsx
+++ packages/web/src/context/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useCallback, ReactNode } from 'react';
+import { createContext, useContext, useState, useCallback, ReactNode } from 'react';
 import { extractUserFromToken } from '../utils/jwtUtils';
 
 export interface UserInfo {
