6b312eefad57c87a89a7052b19b19004312478ae
Igor Siergiej <igorsiergiej@gmail.com>
2025-08-24 15:34:36 +0100

    same-origin requests for images

--- packages/web/src/components/Image/index.tsx
+++ packages/web/src/components/Image/index.tsx
@@ -1,7 +1,6 @@
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import { useState } from 'react';
 
-import { origin } from '../../api/makeRequest';
 import useStyles from './index.styles';
 
 export interface ImageProps {
@@ -11,7 +10,7 @@ export interface ImageProps {
 export const Image: React.FC<ImageProps> = ({ imageId }) => {
     const { classes } = useStyles();
     const [error, setError] = useState(false);
-    const imgSrc = `${origin}/api/image/${imageId}`;
+    const imgSrc = `/api/image/${imageId}`;
 
     if (error) {
         return (<ErrorOutlineIcon />);
