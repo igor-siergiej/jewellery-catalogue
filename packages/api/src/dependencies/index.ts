0228ca7a08de2211027e4fcc4502248e799e9442
Igor Siergiej <igorsiergiej@gmail.com>
2026-01-26 21:53:10 +0000

    fix: linting

--- packages/api/src/dependencies/index.ts
+++ packages/api/src/dependencies/index.ts
@@ -13,79 +13,79 @@ import { type Dependencies, DependencyToken } from './types';
 export const dependencyContainer = DependencyContainer.getInstance<Dependencies>();
 
 export const registerDepdendencies = () => {
-  // Core infrastructure services
-  dependencyContainer.registerSingleton(DependencyToken.Database, MongoDbConnection);
+    // Core infrastructure services
+    dependencyContainer.registerSingleton(DependencyToken.Database, MongoDbConnection);
 
-  dependencyContainer.registerSingleton(DependencyToken.Logger, Logger);
-  dependencyContainer.registerSingleton(DependencyToken.Bucket, ObjectStoreConnection);
-  dependencyContainer.registerSingleton(DependencyToken.IdGenerator, UuidGenerator);
+    dependencyContainer.registerSingleton(DependencyToken.Logger, Logger);
+    dependencyContainer.registerSingleton(DependencyToken.Bucket, ObjectStoreConnection);
+    dependencyContainer.registerSingleton(DependencyToken.IdGenerator, UuidGenerator);
 
-  // Infrastructure adapters
-  dependencyContainer.registerSingleton(
-    DependencyToken.ImageStore,
-    class {
-      constructor() {
-        return new BucketStore(dependencyContainer.resolve(DependencyToken.Bucket));
-      }
-    } as any
-  );
+    // Infrastructure adapters
+    dependencyContainer.registerSingleton(
+        DependencyToken.ImageStore,
+        class {
+            constructor() {
+                return new BucketStore(dependencyContainer.resolve(DependencyToken.Bucket));
+            }
+        } as any
+    );
 
-  // Repositories
-  dependencyContainer.registerSingleton(
-    DependencyToken.DesignRepository,
-    class {
-      constructor() {
-        return new MongoDesignRepository(dependencyContainer.resolve(DependencyToken.Database));
-      }
-    } as any
-  );
+    // Repositories
+    dependencyContainer.registerSingleton(
+        DependencyToken.DesignRepository,
+        class {
+            constructor() {
+                return new MongoDesignRepository(dependencyContainer.resolve(DependencyToken.Database));
+            }
+        } as any
+    );
 
-  dependencyContainer.registerSingleton(
-    DependencyToken.MaterialRepository,
-    class {
-      constructor() {
-        return new MongoMaterialRepository(dependencyContainer.resolve(DependencyToken.Database));
-      }
-    } as any
-  );
+    dependencyContainer.registerSingleton(
+        DependencyToken.MaterialRepository,
+        class {
+            constructor() {
+                return new MongoMaterialRepository(dependencyContainer.resolve(DependencyToken.Database));
+            }
+        } as any
+    );
 
-  // Domain services
-  dependencyContainer.registerSingleton(
-    DependencyToken.MaterialService,
-    class {
-      constructor() {
-        return new MaterialService(
-          dependencyContainer.resolve(DependencyToken.MaterialRepository),
-          dependencyContainer.resolve(DependencyToken.IdGenerator)
-        );
-      }
-    } as any
-  );
+    // Domain services
+    dependencyContainer.registerSingleton(
+        DependencyToken.MaterialService,
+        class {
+            constructor() {
+                return new MaterialService(
+                    dependencyContainer.resolve(DependencyToken.MaterialRepository),
+                    dependencyContainer.resolve(DependencyToken.IdGenerator)
+                );
+            }
+        } as any
+    );
 
-  dependencyContainer.registerSingleton(
-    DependencyToken.ImageService,
-    class {
-      constructor() {
-        return new ImageService(
-          dependencyContainer.resolve(DependencyToken.ImageStore),
-          undefined,
-          dependencyContainer.resolve(DependencyToken.Logger)
-        );
-      }
-    } as any
-  );
+    dependencyContainer.registerSingleton(
+        DependencyToken.ImageService,
+        class {
+            constructor() {
+                return new ImageService(
+                    dependencyContainer.resolve(DependencyToken.ImageStore),
+                    undefined,
+                    dependencyContainer.resolve(DependencyToken.Logger)
+                );
+            }
+        } as any
+    );
 
-  dependencyContainer.registerSingleton(
-    DependencyToken.DesignService,
-    class {
-      constructor() {
-        return new DesignService(
-          dependencyContainer.resolve(DependencyToken.DesignRepository),
-          dependencyContainer.resolve(DependencyToken.ImageService),
-          dependencyContainer.resolve(DependencyToken.IdGenerator),
-          dependencyContainer.resolve(DependencyToken.MaterialRepository)
-        );
-      }
-    } as any
-  );
+    dependencyContainer.registerSingleton(
+        DependencyToken.DesignService,
+        class {
+            constructor() {
+                return new DesignService(
+                    dependencyContainer.resolve(DependencyToken.DesignRepository),
+                    dependencyContainer.resolve(DependencyToken.ImageService),
+                    dependencyContainer.resolve(DependencyToken.IdGenerator),
+                    dependencyContainer.resolve(DependencyToken.MaterialRepository)
+                );
+            }
+        } as any
+    );
 };
